2a2e0bfeb0ffd9ae134b16f9e33d21291e8fe34a
Samuel <idarker112@gmail.com>
2022-11-02 21:14:18 +0100

    first commit

---
+++ chip8_emulator/chip8_core/src/lib.rs
@@ -0,0 +1,44 @@
+const RAM_SIZE: usize = 4096;
+pub const SCREEN_WIDTH: usize = 64;
+pub const SCREEN_HEIGHT: usize = 32;
+const NUM_REGS: usize = 16;
+const STACK_SIZE: usize = 16;
+const NUM_KEYS: usize = 16;
+const START_ADDR: u16 = 0x200;
+
+pub struct Emu {
+    pc: u16,
+    ram: [u8; RAM_SIZE],
+    screen: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
+    v_reg: [u8; NUM_REGS],
+    i_reg: u16,
+    sp: u8,
+    stack: [u16; STACK_SIZE],
+    keys: [bool; NUM_KEYS],
+    delay_timer: u8,
+    sound_timer: u8,
+}
+
+impl Emu {
+    pub fn new() -> Self {
+        Self {
+            pc: START_ADDR,
+            ram: [0; RAM_SIZE],
+            screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
+            v_reg: [0; NUM_REGS],
+            i_reg: 0,
+            sp: 0,
+            stack: [0; STACK_SIZE],
+            keys: [false; NUM_KEYS],
+            delay_timer: 0,
+            sound_timer: 0,
+        }
+    }
+    fn push(&mut self, val: u16) {
+        self.stack[self.sp as usize] = val;
+    }
+    fn pop(&mut self) -> u16 {
+        self.sp -= 1;
+        self.stack[self.sp as usize]
+    }
+}
